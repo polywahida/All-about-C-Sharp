ad190a0467fc56a56930d23bdd3ef94eb6b0c221
poly_wahida <ifrahpoly@gmail.com>
2023-06-03 03:06:58 +0600

    C#-functions and methods.

---
+++ C#Methods/C#Methods/ConvertToUpper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Methods
+{
+    public class ConvertToUpper
+    {
+        public void NameToUpper()
+        {
+            string takeInput = null;
+            Console.WriteLine("Please type your name and press enter : ");
+            takeInput = Console.ReadLine();
+            int trypass = 0;
+            while(Int32.TryParse(takeInput, out trypass))
+            {
+                Console.WriteLine("Please Type String Value");
+                takeInput = Console.ReadLine() ;
+            }
+            string myNameUpperCase = null;
+            if(!string.IsNullOrWhiteSpace(takeInput))
+            {
+                myNameUpperCase = takeInput.ToUpper();
+                Console.WriteLine($"You converted name to upper {myNameUpperCase}");
+            }
+            else
+            {
+                Console.WriteLine("You have added an empty value");
+            }
+        }
+        
+
+    }
+}
---
+++ C#Types and methods/C#Types and methods/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+ 
+int AddNumber(int number1, int number2)
+{
+    return number1 + number2;
+}
+
+
+var sum = AddNumber(2,2);
+
+Console.WriteLine(sum);
