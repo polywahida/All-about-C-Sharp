4a399fcded08b06a6e15caa0aa294e79f69217e4
poly_wahida <ifrahpoly@gmail.com>
2023-06-03 04:01:55 +0600

    C#Decision making and repeat operation

---
+++ Csharp-basics- Decision Making & Repeat Operation/Csharp-basics- Decision Making & Repeat Operation/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Csharp_basics__Decision_Making___Repeat_Operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple if-else condition
+
+//int voteCount = 0;
+//string? result = null;
+
+//voteCount = 150;
+//if (voteCount > 0)
+// {
+//   result = "Won";
+//}
+//else
+//{  result = "Lost";
+////}
+//var result:string = voteCount > 0? "Won" : "Lost";
+////Console.WriteLine($"You have {result}");
+
+// MUlti Nested If else
+//int number  = 0;
+// string? numberResult = null;
+// if (number > 0)
+//{
+///     if (number % 2 == 0)
+//   {
+//     numberResult = "Positive Even Number";
+//
+//  } else
+//            {
+//                numberResult = "Positive Odd Number"; 
+//            } 
+//        } else if (number < 0)
+//        {
+//            numberResult = "Negative Number";
+
+//        }
+//         else
+//        {
+//             numberResult = "Zero";
+
+//        }   var numberResult:string = number > 0 ? number % 2 == 0 ? "Positive Even Number" : "Positive Odd Number"
+//            number < 0 ? "Negative Number" : "Zero";
+
+
+//         Console.WriteLine($"Number {number} is {numberResult}");
+//    }
+//}
+
+
+            ////multi branching
+
+//var grade = score>= 90? "Outstanding":
+//score>=80 ? "Good":
+//score>=70 ? "Average":
+//score>=60 ? "Below Average": "Bad performer";
