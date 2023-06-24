259c7d86aac9cd8a9e432c125e522c7739ebaf51
poly_wahida <ifrahpoly@gmail.com>
2023-06-25 00:19:34 +0600

    Advance OOP

---
+++ AdvanceOOP/AdvanceOOP/Duck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceOOP
+{    
+    //Is-A (Relationship)
+    //Duck is a Bird
+
+    public class Duck: Birdparent
+    {
+        public bool isFly { get; set; } = true;
+        public Duck(string name,string color,string diet) :base(name,color,diet){ 
+        }
+
+        public override void Fly()
+        { Console.WriteLine($"{Name} Can not fly");
+        //  base.Fly(); 
+        }
+         
+    }
+
+
+}
---
+++ AdvanceOOP/AdvanceOOP/Titanic/Titanic/SecondClassPassenger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Titanic
+{
+    public class SecondClassPassenger:Passenger
+    { public SecondClassPassenger(string name, int floorNo, int roomNo) : base(name, floorNo, roomNo)
+
+
+    }
+}
